3bdac5ee3c15e0625f2edc8a129743b7fe4cf24d
cg37 <cg37@outlook.com>
2023-01-14 11:17:45 +0800

    1_twosum

---
+++ 1_Two Sun/1_twosum.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int *array=(int*)malloc(2*sizeof(int)); 
+    for(int i=0;i<numsSize;i++){
+        for(int j=i+1;j<numsSize;j++){
+            if(nums[i]+nums[j]==target){
+                array[0]=i;
+                array[1]=j;
+                *returnSize=2;
+                return array;
+            }         
+        }
+    }
+    *returnSize=0;
+    //*array=NULL;
+    return array;
+}
+int main()
+{
+    int i;
+    int nums[]={2,7,11,15};
+
+    int target=9;
+    int* returnSize = (int*)malloc(sizeof(int));
+    int *res;
+    res=twoSum(nums,4,target,returnSize);
+    for(int i=0;i < *returnSize;i++)
+    {
+        printf("%d",res[i]);
+
+    }
+    return 0;
+}
