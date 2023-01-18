4f568cf21d891c07fceee9526410ea9fd8deca6c
cg37 <cg37@outlook.com>
2023-01-18 23:59:20 +0800

    nothing change

--- test.c
+++ test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
+    char arr[26];
+    memset(arr,0,sizeof(arr));
+    printf("sizeof(arr)=%d\n",sizeof(arr));
     printf("hello World");
     return 0;
 }
